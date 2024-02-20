495dcd4f5913a0ce8a713414abdc5bd95efe2a70
imoleFolio <dremkay71@gmail.com>
2024-02-20 17:19:27 +0100

    task 4: creates a test db, test user and grants stated privileges

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- Do same for test database and test user
+
+--Create the database
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+
+-- Creates a  test user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grants all privileges to test user on the db
+GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+
+-- Grants select privilege on performance_schema to test user
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
