fb23399ebaf502c1b36e4612251e12d2369c68f0
imoleFolio <dremkay71@gmail.com>
2024-02-21 13:40:45 +0100

    task=-3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grants all privileges to test user on the db
-GRANT ALL PRIVILEGE ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grants select privilege on performance_schema to test user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
