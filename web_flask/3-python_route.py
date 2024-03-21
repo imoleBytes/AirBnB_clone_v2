c343100621bc485b7dc24fd20c498c683e7e23d7
imoleFolio <dremkay71@gmail.com>
2024-03-21 13:37:43 +0100

    task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" This module starts a Flask app
+The app listens on 0.0.0.0 at port 5000.
+route ==> /: Displays "Hello HBNB"
+route ==> /hbnb: display "HBNB"
+route ==> /c/<text>: display “C ” followed by the value of the text variable
+                    (replace underscore _ symbols with a space )
+route ==> /python/<text>: display “Python ” followed by the value of the text
+                        variable (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """This function returns 'Hello HBNB'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This function returns 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return f'C {escape(text)}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """display “Python ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return f'Python {escape(text)}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
