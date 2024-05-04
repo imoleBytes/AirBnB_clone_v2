5e4756b3964205f6b4be080b5f2cb33340c74e04
imoleFolio <dremkay71@gmail.com>
2024-05-04 18:25:58 +0100

    task1: restructured

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,9 @@ def do_pack():
     """tar the web_static contents"""
     dt = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = f"versions/web_static_{dt}.tgz"
-    local("mkdir versions", capture=False)
+
+    local("mkdir -p versions/")
     result = local(f"tar -cvzf {file_path} web_static", capture=True)
     if result.succeeded:
-        return "versions/web_static_{dt}.tgz"
-    else:
-        return None
+        return file_path
+    return None
