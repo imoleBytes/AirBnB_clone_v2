cdea7397186cdd66ac9397f3f63b5853ee3f7144
imoleFolio <dremkay71@gmail.com>
2024-05-04 18:12:18 +0100

    task1: made exe

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,16 @@
+#!/usr/bin/python3
+"""fabric module"""
+
 from fabric.api import local
 from datetime import datetime
 
 
-# time0 =date.timetuple(date.today())
-# print(date.today())
-# print("time", time0)
-
-
-# print(dt)
-
 def do_pack():
+    """tar the web_static contents"""
     dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = f"versions/web_static_{dt}.tgz"
     local("mkdir versions", capture=False)
-    result = local(f"tar -cvzf versions/web_static_{dt}.tgz web_static", capture=True)
+    result = local(f"tar -cvzf {file_path} web_static", capture=True)
     if result.succeeded:
         return "versions/web_static_{dt}.tgz"
     else:
